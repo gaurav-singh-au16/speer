4b64e1b2eb19f56047f29b945d02724ef031aca9
Gaurav Singh <gauravsingh.info@gmail.com>
2024-01-03 18:44:57 +0530

    added limitter

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const db = require('./helpers/db.helper')
 const router = require('./routes')
 const bodyParser = require('body-parser');
+const rateLimit = require('express-rate-limit');
 
 
 const app = express()
@@ -9,10 +10,18 @@ const app = express()
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const limiter = rateLimit({
+    windowMs: 60 * 1000,
+    max: 10,
+    message: 'Too many requests from this IP, please try again after a minute',
+});
+
+app.use(limiter);
+
 app.use('/api/', router);
 
 // db connection
-db.sync({alter: true}).then(() => {
+db.sync({ alter: true }).then(() => {
     console.log('Database Connected!')
 }).catch((err) => {
     console.log('Error => ', err)
